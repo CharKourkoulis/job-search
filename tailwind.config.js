ea8dd5f1802c8c2d3bf3b68639931529dc4ca41b
CharKourkoulis <cult_bob@hotmail.com>
2023-05-15 14:49:20 +0300

    feat: created navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts}'],
   theme: {
     extend: {
-      fontFamily: { sans: ['Open Sans', ...defaultTheme.fontFamily.sans] }
+      fontFamily: {
+        sans: ['Open Sans', ...defaultTheme.fontFamily.sans]
+      },
+      colors: {
+        'brand-gray-1': '#dadce0',
+        'brand-blue-1': '#1967d2',
+        'brand-green-1': '#137333'
+      }
     }
   },
   plugins: []
