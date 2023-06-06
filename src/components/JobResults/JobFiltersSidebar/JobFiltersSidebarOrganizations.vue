198a89279b5e2d2e7164c357a57d535a5a762768
CharKourkoulis <cult_bob@hotmail.com>
2023-06-06 14:28:41 +0300

    feat: added collapsible accordion component

---
+++ src/components/JobResults/JobFiltersSidebar/JobFiltersSidebarOrganizations.vue
@@ -0,0 +1,24 @@
+<template>
+  <collapsible-accordion header="Organizations">
+    <div class="mt-5">
+      <fieldset>
+        <ul class="flex flex-row flex-wrap">
+          <li class="h-8 w-1/2">
+            <input id="vueTube" type="checkbox" class="mr-3" />
+            <label for="vueTube">VueTube</label>
+          </li>
+        </ul>
+      </fieldset>
+    </div>
+  </collapsible-accordion>
+</template>
+
+<script>
+import CollapsibleAccordion from '@/components/Shared/CollapsibleAccordion.vue'
+export default {
+  name: 'JobFiltersSidebarOrganizations',
+  components: {
+    CollapsibleAccordion
+  }
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { faSearch, faAngleDown, faAngleUp } from '@fortawesome/free-solid-svg-icons'
 import router from '@/router'
 
 library.add(faSearch)
+library.add(faAngleDown)
+library.add(faAngleUp)
 
 const pinia = createPinia()
 createApp(App).use(pinia).use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
